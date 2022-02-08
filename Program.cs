4bc845d95086626d3208d9898072686422467a1b
Yılmaz Alpaslan <77583632+Yilmaz4@users.noreply.github.com>
2022-02-08 22:39:15 +0300

    Add DoMove function, implement first move

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace TicTacToe
 {
     class Program
@@ -22,6 +25,10 @@ namespace TicTacToe
             public int GetCell(int x, int y)
             {
                 return board[x][y];
+            }
+            public List<List<int>> Raw()
+            {
+                return board;
             }
             public void SetCell(int x, int y, int val)
             {
@@ -58,20 +65,86 @@ namespace TicTacToe
                 Console.ResetColor();
             }
         }
-        private static void DecideMove(Board board)
+        private static void DoMove(Board board)
         {
-
+            List<bool> boardEmpty = new();
+            foreach (List<int> subboard in board.Raw())
+            {
+                if (subboard.Any(o => o != subboard[0]) & Convert.ToBoolean(subboard[0])) boardEmpty.Add(true);
+                else boardEmpty.Add(false);
+            }
+            if (boardEmpty.Count == 3 & boardEmpty.Any(o => o != boardEmpty[0]))
+            {
+                List<List<int>> moves = new()
+                {
+                    new List<int>()
+                    {
+                        0, 0
+                    },
+                    new List<int>()
+                    {
+                        0, 2
+                    },
+                    new List<int>()
+                    {
+                        2, 0
+                    },
+                    new List<int>()
+                    {
+                        2, 2
+                    },
+                };
+                Random random = new();
+                List<int> move = moves[random.Next(moves.Count)];
+                board.SetCell(move[0], move[1], 1);
+            }
+        }
+        public static int Compare(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+            if (n == 0)
+            {
+                return m;
+            }
+            if (m == 0)
+            {
+                return n;
+            }
+            for (int i = 0; i <= n; d[i, 0] = i++)
+                ;
+            for (int j = 0; j <= m; d[0, j] = j++)
+                ;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            return d[n, m];
         }
         public static void Main(string[] args)
         {
             Board board = new();
             RenderBoard(board);
-            Console.Write("\nWelcome to TicTacToe! Who should start first? ");
-            string? input = Console.ReadLine();
-            List<string> inputs = new() {"Computer", "Me"};
-            if (inputs.OrderBy(s => string.Compare(s, input)).First() == "You")
+            while (true)
             {
+                Console.Write("\nWelcome to TicTacToe! Who should start first? ");
+                string? input = Console.ReadLine();
 
+                if (true) ;
+
+                while (false)
+                {
+                    Console.WriteLine("You've entered an invalid input! It can be either \"Computer\" or \"Me\" which is you.\nPress any key to continue . . .");
+                    Console.ReadKey();
+                    continue;
+                }
             }
         }
     }
