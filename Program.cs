1c7d7033c3d1e6163d752a6b647df6585c1967aa
Yılmaz Alpaslan <77583632+Yilmaz4@users.noreply.github.com>
2022-02-09 21:52:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+#pragma warning disable CS8602
+
 using System;
 using System.Collections.Generic;
 
 namespace TicTacToe
 {
-    class Program
+    public static class Program
     {
         public class Board
         {
@@ -34,46 +36,88 @@ namespace TicTacToe
             {
                 board[x][y] = val;
             }
+            public bool IsCellEmpty(int x, int y)
+            {
+                return !Convert.ToBoolean(board[x][y]);
+            }
+            public bool IsEmpty()
+            {
+                List<bool> boardEmpty = new();
+                foreach (List<int> subboard in board)
+                {
+                    if (CheckEqualityOfAll(subboard) & subboard[0] == 0) boardEmpty.Add(true);
+                    else boardEmpty.Add(false);
+                }
+                return (boardEmpty.Count == 3 & CheckEqualityOfAll(boardEmpty) & boardEmpty[0]);
+            }
         }
         private static void RenderBoard(Board board)
         {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.SetCursorPosition(0, 1);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, 0);
+            }
             Dictionary<int, string> marks = new()
             {
                 {0, " "},
                 {1, "x"},
                 {2, "o"}
             };
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("    1   2   3");
             for (int i = 0; i < 3; i++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 if (i != 3)
                 {
-                    Console.WriteLine(i == 0 ? "┌───┬───┬───┐" : "├───┼───┼───┤");
+                    Console.WriteLine(i == 0 ? "  ┌───┬───┬───┐" : "  ├───┼───┼───┤");
                 } 
                 Console.ResetColor();
                 for (int j = 0; j < 3; j++)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.Write("│ ");
+                    Console.Write(!Convert.ToBoolean(j) ? $"{i + 1} │ " : "│ ");
                     Console.ResetColor();
                     Console.Write(marks[board.GetCell(i, j)]);
                     Console.ForegroundColor= ConsoleColor.DarkGray;
                     Console.Write((j == 2 ? " │\n" : " "));
                 }
                 Console.ForegroundColor= ConsoleColor.DarkGray;
-                if (i == 2) Console.WriteLine("└───┴───┴───┘");
+                if (i == 2) Console.WriteLine("  └───┴───┴───┘");
                 Console.ResetColor();
             }
+        }
+        private static bool CheckEqualityOfAll<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+            using (var enumerator = enumerable.GetEnumerator())
+            {
+                var toCompare = default(T);
+                if (enumerator.MoveNext())
+                {
+                    toCompare = enumerator.Current;
+                }
+                while (enumerator.MoveNext())
+                {
+                    if (toCompare != null && !toCompare.Equals(enumerator.Current))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
         private static void DoMove(Board board)
         {
             List<bool> boardEmpty = new();
             foreach (List<int> subboard in board.Raw())
             {
-                if (subboard.Any(o => o != subboard[0]) & Convert.ToBoolean(subboard[0])) boardEmpty.Add(true);
+                if (CheckEqualityOfAll(subboard) & subboard[0] == 0) boardEmpty.Add(true);
                 else boardEmpty.Add(false);
             }
-            if (boardEmpty.Count == 3 & boardEmpty.Any(o => o != boardEmpty[0]))
+            if (boardEmpty.Count == 3 & CheckEqualityOfAll(boardEmpty) & boardEmpty[0])
             {
                 List<List<int>> moves = new()
                 {
@@ -98,8 +142,12 @@ namespace TicTacToe
                 List<int> move = moves[random.Next(moves.Count)];
                 board.SetCell(move[0], move[1], 1);
             }
+            else if (Convert.ToBoolean(board.Raw()[0][0]))
+            {
+
+            }
         }
-        public static int Compare(string s, string t)
+        private static int Compare(string s, string t)
         {
             int n = s.Length;
             int m = t.Length;
@@ -132,13 +180,24 @@ namespace TicTacToe
         {
             Board board = new();
             RenderBoard(board);
+
             while (true)
             {
                 Console.Write("\nWelcome to TicTacToe! Who should start first? ");
-                string? input = Console.ReadLine();
+                string? input = Console.ReadLine().ToUpper();
 
-                if (true) ;
+                Console.WriteLine(input);
 
+                if (input == "ME")
+                {
+
+                }
+                else if (input == "COMPUTER" || input == "PC" || input == "AI")
+                {
+                    Console.Clear();
+                    DoMove(board);
+                    RenderBoard(board);
+                }
                 while (false)
                 {
                     Console.WriteLine("You've entered an invalid input! It can be either \"Computer\" or \"Me\" which is you.\nPress any key to continue . . .");
